b08293c6fd2e6ba06eec6af276f6f6f1588ccdd5
Slava Bezgachev <bezgachev@gmail.com>
2018-10-23 20:20:26 +0600

    Adding tray

---
+++ boilerplates/tasky/index.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const electron = require("electron");
+
+const { app, BrowserWindow, Tray } = electron;
+
+let mainWindow;
+let tray;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    height: 500,
+    width: 300,
+    frame: false,
+    resizable: false,
+    show: false
+  });
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+  const iconName = process.platform === "win32" ? "windows-icon.png" : "iconTemplate.png";
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+  tray = new Tray(iconPath);
+});
