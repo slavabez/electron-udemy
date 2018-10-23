848afcd253cef8c08b81548bc0b1f44ffc67b641
Slava Bezgachev <bezgachev@gmail.com>
2018-10-23 22:32:05 +0600

    Tray works, but positioning os off on linux

--- boilerplates/tasky/index.js
+++ boilerplates/tasky/index.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const electron = require("electron");
 
-const { app, BrowserWindow, Tray } = electron;
+const { app, BrowserWindow, Tray, screen } = electron;
 
 let mainWindow;
 let tray;
@@ -16,7 +16,26 @@ app.on("ready", () => {
   });
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 
-  const iconName = process.platform === "win32" ? "windows-icon.png" : "iconTemplate.png";
+  const iconName =
+    process.platform === "win32" ? "windows-icon.png" : "iconTemplate.png";
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
   tray = new Tray(iconPath);
+  tray.on("click", (event, bounds) => {
+    const { x, y } = bounds;
+    const { height, width } = mainWindow.getBounds();
+
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      const yPos = process.platform === "darwin" ? y : y - height;
+      mainWindow.setBounds({
+        x: x - width / 2,
+        y: yPos,
+        height,
+        width
+      });
+      mainWindow.show();
+      console.log(mainWindow.getBounds(), screen);
+    }
+  });
 });
